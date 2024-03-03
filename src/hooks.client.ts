c6033874a98f5460aec4099fd497814f74439e3f
hualin.zhu <neo.js.cn@gmail.com>
2024-03-03 18:44:04 +0800

    Update hooks.client.ts

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -4,7 +4,9 @@ import { currentUser, pb } from '$lib/pocketbase';
 try {
 	pb.authStore.loadFromCookie(document.cookie); // load it first
 	// get an up-to-date auth store state by veryfing and refreshing the loaded auth model (if any)
-	pb.authStore.isValid && (await pb.collection('users').authRefresh());
+	if(pb.authStore.isValid){
+		pb.collection('users').authRefresh()
+   }
 } catch (_) {
 	// clear the auth store on failed refresh
 	pb.authStore.clear();
